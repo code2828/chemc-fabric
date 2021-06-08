cce67284a83c409d55939acfd015f44c0d82aab5
code2828 <stevenhaocheng@163.com>
2021-06-08 15:34:33 +0800

    removed an import

--- src/main/java/com/code2828/mods/CheMC.java
+++ src/main/java/com/code2828/mods/CheMC.java
@@ -12,7 +12,6 @@ import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
